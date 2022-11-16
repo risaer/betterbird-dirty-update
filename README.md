2831e29b412a22a1a9b09cef25d3b56112f05432
Holger <git.aufderpalme@xoxy.net>
2022-11-16 19:15:49 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # betterbird-dirty-update
 Quick and Dirty Install/Update Betterbird on linux
+
+I have only tried it with v. 102 and i cannot garantee for proper functionality.
+
+# I M P O R T A N T
+I have written it for myself and for me it works (now). Maybe it does not work for you. Maybe betterbird changes something somewhen.
+This is not for beginners. Inspect the code and adapt it to your needs!
+
+*It might be faulty. Use this on your own risk*
+
+
+# What it does
+- Downloads the archive from the betterbird website
+- checks the sh256
+- extracts it to your desired directory (default is `~/betterbird` )
+- creates a .desktop file
+
+
+## Howto
+
+* clone repo or simple download the script and make it executable.
+* open the script and adapt variables
+* run it
+* if everything went well:
+    * betterbird is updated
+    * a .desktop file is ~/Betterbird.desktop which can be copied to your desktop (you might need to mark it as trusted)
+
+
+
+
+
---
+++ updateBetterbird.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# get Download from: https://www.betterbird.eu/downloads/
+# find the suitable language (en-US,de,fr,...): 
+lang=de
+#release, previous:
+version=release
+
+
+tmpPath=~/tmp/betterbird
+tmpFile=$tmpPath/download
+
+dstRootPath=~/
+desktopFile=$dstRootPath/betterbird.desktop
+
+mkdir -p $tmpPath
+cd  $tmpPath||exit 1
+
+downloadUpdate(){
+	rm $tmpFile
+	wget -O $tmpFile "https://www.betterbird.eu/downloads/get.php?os=linux&lang=$lang&version=$version"|grep Location:
+#	Location: https://www.betterbird.eu/downloads/LinuxArchive/betterbird-102.5.0-bb24.de.linux-x86_64.tar.bz2 [following]
+
+}
+
+
+
+checkHash(){
+	hash=`sha256sum $tmpFile|awk '{print $1}'`
+	echo Hash: $hash
+	update=`cat sha256*.txt|grep $hash|awk '{print $2}'`
+	echo Found: $update
+	if [ "$update" == "" ]; then
+		echo getting last version of hash
+		rm $tmpPath/sha256*.txt
+	        wget https://www.betterbird.eu/downloads/sha256-102.txt
+		
+		hash=`sha256sum $tmpFile|awk '{print $1}'`
+		echo Hash: $hash
+		update=`cat sha256*.txt|grep $hash|awk '{print $2}'`
+		echo Found: $update
+		if [ "$update" == "" ]; then
+			echo no valid update found
+			echo check this script $0 and the downloadpage. Perhaps you have to adapt the sha256-Url
+			exit 1
+		fi
+
+	fi
+}
+
+
+extract(){
+	cd $dstRootPath||exit 1
+	rm -rf betterbird
+	tar xjvf $tmpFile -C $dstRootPath
+	cd betterbird
+	ls -l
+	pwd
+
+}
+
+createDesktopFile(){
+cat << EOF > $desktopFile
+	[Desktop Entry]
+	Encoding=UTF-8
+	Version=1.0
+	Type=Application
+	Terminal=false
+	Name=Betterbird
+EOF
+	echo Exec=$dstRootPath/betterbird/betterbird >> $desktopFile
+	echo Icon=$dstRootPath/betterbird//chrome/icons/default/default256.png >> $desktopFile
+
+#	sudo mv ~/.local/share/applications/<application-name.desktop> /usr/share/applications/
+}
+
+
+
+downloadUpdate
+getSha256
+checkHash
+extract
+createDesktopFile
+echo Ende
